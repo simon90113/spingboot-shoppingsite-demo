2a47d7e9d9e2c2ef6b6263be6742733d53bb13d9
simon90113 <63653525+simon90113@users.noreply.github.com>
2021-02-23 00:44:24 +0800

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import com.example.demo.model.User;
 
 public interface UserService {
-	
+	 
 	Optional<User> findByUsername(String username);
 	
 	Optional<User> findByEmail(String email);
